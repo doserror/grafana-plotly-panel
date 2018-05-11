08926c2cd9847dcafbcc6a0d4e690c89adeeba32
Alexander Papratilov <doserror@gmail.com>
2018-05-11 03:20:39 +0300

    Revert "Added the Surface option as menu item"
    This reverts commit 210ea4525d22ea935eafac314f0f27d75a3a2090.

--- src/module.ts
+++ src/module.ts
@@ -522,7 +522,7 @@ class PlotlyPanelCtrl extends MetricsPanelCtrl {
       this.trace.x = dX.points;
       this.trace.y = dY.points;
 
-      if (cfg.settings.type === 'scatter3d' || cfg.settings.type === 'surface') {
+      if (cfg.settings.type === 'scatter3d') {
         dZ = this.data[mapping.z];
         if (!dZ) {
           throw {message: 'Unable to find Z: ' + mapping.z};
